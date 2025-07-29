06707e87899a3a0b159dd2f821f49c9ec177097f
Salfahin <quentinhenrard0@gmail.com>
2025-07-29 14:56:38 +0200

    Adding "configurations" page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 export default function Home() {
   return (
+  <div>
    <h1>Pc Building Helper</h1>
+   <p> /composants/cpu</p>
+   <p> /composants/motherboard</p>
+   <p> /configurations </p>
+  </div>
   );
 }
