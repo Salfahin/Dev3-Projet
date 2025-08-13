9f7e1d4d00761ccda0877532d710ca38acb9fc23
jwillems202415 <j.willems@students.ephec.be>
2025-08-13 15:32:04 +0200

    Added API routes to fetch latest parts.

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -8,6 +8,8 @@ import { Part } from './types/part';
 import { Configuration } from './types/configuration';
 import { FetchParts } from './queries/fetchParts';
 import { FetchConfigs } from './queries/fetchConfigs';
+import { Product } from './types/product';
+import { FetchLatest } from './queries/fetchLatest';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -23,6 +25,131 @@ app.use(express.json());
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, Express with TypeScript!');
 });
+
+// HOME PAGE
+
+// Configurations section
+app.get('/api/latest/configurations', async (req: Request, res: Response) => {
+  try {
+    const products: Product[] = await FetchLatest();
+    res.json(products);
+  } catch (error) {
+    console.error(':(\nFailed to fetch latest products.');
+    res.status(500).json({ error: 'Internal server error' });
+  };
+});
+
+// Processors section
+app.get('/api/latest/processors', async (req: Request, res: Response) => {
+  try {
+    const products: Product[] = await FetchLatest(0);
+    res.json(products);
+  } catch (error) {
+    console.error(':(\nFailed to fetch latest processors.');
+    res.status(500).json({ error: 'Internal server error' });
+  };
+});
+
+// Coolers section
+app.get('/api/latest/coolers', async (req: Request, res: Response) => {
+  try {
+    const products: Product[] = await FetchLatest(1);
+    res.json(products);
+  } catch (error) {
+    console.error(':(\nFailed to fetch latest coolers.');
+    res.status(500).json({ error: 'Internal server error' });
+  };
+});
+
+// Motherboards section
+app.get('/api/latest/motherboards', async (req: Request, res: Response) => {
+  try {
+    const products: Product[] = await FetchLatest(4);
+    res.json(products);
+  } catch (error) {
+    console.error(':(\nFailed to fetch latest motherboards.');
+    res.status(500).json({ error: 'Internal server error' });
+  };
+});
+
+// Memory section
+app.get('/api/latest/memory', async (req: Request, res: Response) => {
+  try {
+    const products: Product[] = await FetchLatest(5);
+    res.json(products);
+  } catch (error) {
+    console.error(':(\nFailed to fetch latest memory.');
+    res.status(500).json({ error: 'Internal server error' });
+  };
+});
+
+// Disks section
+app.get('/api/latest/disks', async (req: Request, res: Response) => {
+  try {
+    const products: Product[] = await FetchLatest(6);
+    res.json(products);
+  } catch (error) {
+    console.error(':(\nFailed to fetch latest disks.');
+    res.status(500).json({ error: 'Internal server error' });
+  };
+});
+
+// Video cards section
+app.get('/api/latest/video-cards', async (req: Request, res: Response) => {
+  try {
+    const products: Product[] = await FetchLatest(2);
+    res.json(products);
+  } catch (error) {
+    console.error(':(\nFailed to fetch latest video cards.');
+    res.status(500).json({ error: 'Internal server error' });
+  };
+});
+
+// Cases section
+app.get('/api/latest/cases', async (req: Request, res: Response) => {
+  try {
+    const products: Product[] = await FetchLatest(7);
+    res.json(products);
+  } catch (error) {
+    console.error(':(\nFailed to fetch latest cases.');
+    res.status(500).json({ error: 'Internal server error' });
+  };
+});
+
+// Case fans section
+app.get('/api/latest/case-fans', async (req: Request, res: Response) => {
+  try {
+    const products: Product[] = await FetchLatest(8);
+    res.json(products);
+  } catch (error) {
+    console.error(':(\nFailed to fetch latest case fans.');
+    res.status(500).json({ error: 'Internal server error' });
+  };
+});
+
+// Power supplies section
+app.get('/api/latest/power-supplies', async (req: Request, res: Response) => {
+  try {
+    const products: Product[] = await FetchLatest(3);
+    res.json(products);
+  } catch (error) {
+    console.error(':(\nFailed to fetch latest power supplies.');
+    res.status(500).json({ error: 'Internal server error' });
+  };
+});
+
+// Others section
+app.get('/api/latest/others', async (req: Request, res: Response) => {
+  try {
+    const products: Product[] = await FetchLatest(9);
+    res.json(products);
+  } catch (error) {
+    console.error(':(\nFailed to fetch latest others.');
+    res.status(500).json({ error: 'Internal server error' });
+  };
+});
+
+// PRODUCTS PAGES
 
 //Configurations page.
 app.get('/api/configurations', async (req: Request, res: Response) => {
---
+++ Backend/src/types/product.ts
@@ -0,0 +1,7 @@
+// This is a generic product. It contains everything common to all types, does not make any distinction.
+
+export type Product = {
+  id: number;
+  name: string;
+  price: number;
+};
