dacff3ea592a036d204c6690df46da738420ca52
Salfahin <quentinhenrard0@gmail.com>
2025-08-25 21:19:53 +0200

    Editing Supabaseclient to switch to Service Key

--- Backend/src/lib/supabaseClient.ts
+++ Backend/src/lib/supabaseClient.ts
@@ -1,19 +1,16 @@
+// this file initializes the Supabase Client
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnon = process.env.SUPABASE_ANON_KEY;
 const supabaseService = process.env.SUPABASE_SERVICE_KEY;
 
-if (!supabaseUrl || !supabaseAnon || !supabaseService) {
+if (!supabaseUrl || !supabaseService) {
   console.error('⚠️  Supabase environment variables are missing!');
-  console.error({ SUPABASE_URL: supabaseUrl, SUPABASE_ANON_KEY: !!supabaseAnon, SUPABASE_SERVICE_ROLE_KEY: !!supabaseService });
+  console.error({ SUPABASE_URL: supabaseUrl, SUPABASE_SERVICE_ROLE_KEY: !! supabaseService });
   process.exit(1); // stoppe le backend proprement
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnon, {
+export const supabase = createClient(supabaseUrl, supabaseService, {
   auth: { persistSession: false, autoRefreshToken: false }
 });
 
-export const supabaseAdmin = createClient(supabaseUrl, supabaseService, {
-  auth: { persistSession: false, autoRefreshToken: false }
-});
