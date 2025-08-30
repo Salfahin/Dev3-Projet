f41c9643b7141b4259770ad49540cec176ad43b1
jwillems202415 <j.willems@students.ephec.be>
2025-08-30 11:38:24 +0200

    Fixed API URL

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,3 +1,3 @@
-// export const API_URL = "http://188.166.38.93:3001";
+export const API_URL = "http://188.166.38.93:3001";
 
-export const API_URL = "http://localhost:3001";
+// export const API_URL = "http://localhost:3001";
